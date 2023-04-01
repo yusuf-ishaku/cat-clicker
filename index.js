cfdcf8dea49af626c279f042a236884e650b0e11
yusuf <kingshakzy@gmail.com>
2023-04-01 23:03:28 +0100

    Basic functionality added

--- index.js
+++ index.js
@@ -3,5 +3,7 @@ let display = document.querySelector(".count");
 let click = 0;
 display.textContent = click;
 clicker.addEventListener("click", () =>{
-    click++
-})
+    click = click+ 1;
+    console.log(click);
+    display.textContent = click;
+}, false)
