8fbef7064c570e83fbd8dbf3b0f9719d30ab15a1
yusuf <kingshakzy@gmail.com>
2023-04-01 23:25:33 +0100

    Expectations changed

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
-let clicker = document.querySelector(".click");
-let display = document.querySelector(".count");
+let clicker = document.querySelectorAll(".click");
+console.log(clicker)
+let display = document.querySelectorAll(".count");
 let click = 0;
-display.textContent = click;
-clicker.addEventListener("click", () =>{
-    click = click+ 1;
-    console.log(click);
-    display.textContent = click;
-}, false)
+let click1 = 0;
+display[0].textContent = click;
+display[1].textContent = click1
+clicker[0].addEventListener("click", () =>{
+    click++;
+    display[0].textContent = click;
+});
+clicker[1].addEventListener("click", () =>{
+    click1++;
+    display[1].textContent = click1
+})
