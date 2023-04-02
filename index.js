adfa417a56ab7e9ab9bafbc4c77022302bb65f58
yusuf <kingshakzy@gmail.com>
2023-04-02 12:16:16 +0100

    New requirements added

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 let clicker = document.querySelectorAll(".click");
+let catSpace = document.querySelectorAll(".cat-img");
+console.log(catSpace)
 console.log(clicker)
 let display = document.querySelectorAll(".count");
-let click = 0;
-let click1 = 0;
-display[0].textContent = click;
-display[1].textContent = click1
-clicker[0].addEventListener("click", () =>{
-    click++;
-    display[0].textContent = click;
+let cats = document.querySelectorAll("li");
+// let click = 0
+cats.forEach((elem, index) =>{
+    elem.addEventListener("click", () =>{
+        for(i = 0; i < clicker.length; i++){
+            catSpace[i].style.display = "none";
+        }
+        catSpace[index].style.display = "block";
+    })
 });
-clicker[1].addEventListener("click", () =>{
-    click1++;
-    display[1].textContent = click1
+catSpace.forEach((elem)=>elem.style.display = "none")
+clicker.forEach((elem, index) =>{
+    let click = 0;
+    elem.addEventListener("click", () =>{
+        click++;
+        display[index].textContent = click
+    })
 })
